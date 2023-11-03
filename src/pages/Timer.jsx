ac2aa03507d70d9880bc0a5c0b04cf612e57f17f
Azka Siddiqui <129907688+azka-siddiqui@users.noreply.github.com>
2023-11-03 19:02:05 -0400

    Update Timer.jsx

--- src/pages/Timer.jsx
+++ src/pages/Timer.jsx
@@ -19,7 +19,7 @@ const countdownStyle = {
   fontSize: "35em",
   fontFamily: "Palatino, URW Palladio L, serif",
   marginTop: "-.05em",
-  marginBottom: "4em",
+  marginBottom: "-.015em",
 };
 
 const countdownGreyStyle = {

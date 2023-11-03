56947eda5b79f7bae173301eb23708e9fc756189
Azka Siddiqui <129907688+azka-siddiqui@users.noreply.github.com>
2023-11-03 19:28:28 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
 import Clock from "../pages/Clock";
-
 const Header = (props) => {
   console.log(props.name);
   return (
@@ -12,12 +11,11 @@ const Header = (props) => {
             alt="Basketball Icon"
             src="https://www.dpcdsb.org/JOESS/PublishingImages/Pages/Forms/AllItems/Jaguar%20head.png"
             className="icon"
-            style={{ width: '80px', height: '80px', marginTop: '10px', marginLeft: '40px'}}
+            style={{ width: '80px', height: '80px', marginTop: '15px', marginLeft: '30px'}}
           />
           <p className="slogan">Go Jags Go!</p>
         </div>
       </nav>
-
       <Link to="/profile" className="name">
         <p>{props.name}</p>
       </Link>
@@ -25,5 +23,4 @@ const Header = (props) => {
     </header>
   );
 };
-
 export default Header;

27b997e65d7baf85fc6e5dc14875ce1a1ef96ad0
Azka Siddiqui <129907688+azka-siddiqui@users.noreply.github.com>
2023-11-03 18:53:07 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,28 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Header.css";
 import Clock from "../pages/Clock";
 
 const Header = (props) => {
-    console.log(props.name)
-    return (
-          <header className="header">
-            <nav>
-                <img
-                  alt="Basketball Icon"
-                  src="https://www.dpcdsb.org/JOESS/PublishingImages/Pages/Forms/AllItems/Jaguar%20head.png"
-                  className="icon"
-                  style={{ width: '100px', height: '100px', marginTop: '15px', marginLeft: '35px' }}
-            />
-            </nav>
-    
-          <Link to = "/profile" className = "name">
-            <p>{props.name}</p>
-          </Link>
-          <Clock />
-          </header>
+  console.log(props.name);
+  return (
+    <header className="header">
+      <nav>
+        <div className="header-content">
+          <img
+            alt="Basketball Icon"
+            src="https://www.dpcdsb.org/JOESS/PublishingImages/Pages/Forms/AllItems/Jaguar%20head.png"
+            className="icon"
+          />
+          <p className="slogan">Go Jags Go!</p>
+        </div>
+      </nav>
 
-          );
+      <Link to="/profile" className="name">
+        <p>{props.name}</p>
+      </Link>
+      <Clock />
+    </header>
+  );
 };
 
 export default Header;

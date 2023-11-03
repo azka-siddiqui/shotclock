e922f5e7780a930fa8aa6d838fa0e6a2e1b47e91
Azka Siddiqui <129907688+azka-siddiqui@users.noreply.github.com>
2023-11-02 22:04:01 -0400

    Update Timer.jsx

--- src/pages/Timer.jsx
+++ src/pages/Timer.jsx
@@ -1,25 +1,25 @@
 import { useState, useEffect } from "react";
 import Header from "../components/Header";
 
+// Styles for various elements
 const buttonStyle = {
-  padding: "0.6em 1.2em",
-  fontSize: "1em",
-  fontWeight: "500",
+  padding: "2% 4%",
+  fontSize: "2vw",
+  fontWeight: "bold",
   fontFamily: "inherit",
   backgroundColor: "white",
   color: "#000",
   cursor: "pointer",
   transition: "background-color 0.25s",
-  borderRadius: "4",
-  margin: "1.0em",
-  marginBottom: "-.08em",
+  borderRadius: "4px",
+  margin: "2%",
 };
 
 const countdownStyle = {
-  fontSize: "35em",
-  fontFamily: "Palatino, URW Palladio L, serif",
-  marginTop: "-.05em",
-  marginBottom: "-.08em",
+  fontSize: "6vw",
+  fontFamily: "Palatino, URW Palladio L, serif", // Apply the custom font
+  marginTop: "-2vh",
+  marginBottom: "-1vh",
 };
 
 const countdownGreyStyle = {
@@ -28,7 +28,7 @@ const countdownGreyStyle = {
 };
 
 const containerStyle = {
-  marginTop: "-2em",
+  marginTop: "-4vh",
   display: "flex",
   flexDirection: "row",
   justifyContent: "center",
@@ -36,7 +36,7 @@ const containerStyle = {
 
 const resetButtonStyle = {
   ...buttonStyle,
-  border: "1px solid #444",
+  border: "2px solid #444",
 };
 
 const clearButtonStyle = {
@@ -44,28 +44,34 @@ const clearButtonStyle = {
 };
 
 const Timer = () => {
+  // State variables for time, paused state, and clear button click
   const [time, setTime] = useState(24);
   const [paused, setPaused] = useState(true);
   const [clearClicked, setClearClicked] = useState(false);
 
+  // Function to toggle the paused state
   const togglePaused = () => {
-    setPaused((paused) => !paused);
+    setPaused(!paused);
   };
 
+  // Function to reset the time
   const resetTime = (newTime) => {
     setTime(newTime);
     setPaused(false);
     setClearClicked(false);
   };
 
+  // Function to clear the time
   const clearTime = () => {
     setTime(0);
     setClearClicked(true);
+    // Automatically reset the "Start" button
     if (!paused) {
       togglePaused();
     }
   };
 
+  // Effect to decrement time when not paused
   useEffect(() => {
     let id;
     if (!paused && time > 0) {
@@ -73,7 +79,7 @@ const Timer = () => {
         if (time === 1) {
           setPaused(true);
         }
-        setTime((time) => time - 1);
+        setTime(time - 1);
       }, 1000);
     }
 
@@ -89,17 +95,15 @@ const Timer = () => {
           {time < 10 ? `0${time}` : time}
         </p>
         <div className="button-container" style={containerStyle}>
-          {!clearClicked && (
-            <button
-              onClick={togglePaused}
-              style={{
-                ...buttonStyle,
-                border: paused ? "1px solid green" : "1px solid red",
-              }}
-            >
-              {paused ? "Start" : "Stop"}
-            </button>
-          )}
+          <button
+            onClick={togglePaused}
+            style={{
+              ...buttonStyle,
+              border: paused ? "2px solid green" : "2px solid red",
+            }}
+          >
+            {paused && !clearClicked ? "Start" : "Stop"}
+          </button>
           <button onClick={() => resetTime(24)} style={resetButtonStyle}>
             Reset 24s
           </button>

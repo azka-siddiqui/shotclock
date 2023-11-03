21ccb993ec5c92aeeedbc58f0038a94c27740f03
Azka Siddiqui <azkasiddiqui240@gmail.com>
2023-11-03 17:03:08 -0400

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import App from "./pages/App.jsx";
-import Profile from "./pages/Profile.jsx";
-import Count from "./pages/Count.jsx";
-import Timer from "./pages/Timer.jsx";
-import Countdown from "./pages/Countdown.jsx";
+import Timer from "./pages/Countdown.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Timer />
-  },
-  {
-    path: "/profile",
-    element: <Profile />
-  },
-  {
-    path: "/count",
-    element: <Count />,
-  },
-  {
-    path: "/App",
-    element: <App />,
-  },
-  {
-    path: "/countdown",
-    element: <Countdown />
   }
 ]);
 
